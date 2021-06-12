d634862756a10f0eb3d69e247c55e5ca5bcf821c
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-12 13:42:36 +0530

    Update Inserting data from userinput.java

--- Inserting data from userinput.java
+++ Inserting data from userinput.java
@@ -1,1 +1,37 @@
+package com.jdbc;
 
+import java.sql.*;
+import java.util.Scanner;
+
+public class E 
+{
+	public static void main(String[] args) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+
+		String url = "jdbc:mysql://localhost:3309/javaep";
+		String uname = "root";
+		String pass  = "admin";
+		
+		System.out.print("\nEnter Name");
+		String Name = sc.nextLine();
+		
+		System.out.print("Enter id");
+		int student_id = sc.nextInt();
+
+
+		String query = "INSERT INTO student VALUES(?,?)";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url,uname,pass);
+		
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1,student_id);
+		st.setString(2,Name);
+		
+		int r = st.executeUpdate();
+		System.out.println(r+" row/s affected");
+		
+			
+	}
+}
